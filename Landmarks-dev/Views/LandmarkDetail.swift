800d695ab1487a116f09758fdcfb4913d0800423
Fangkangkang1126 <461439583@qq.com>
2025-04-22 02:52:59 +0800

    Initial Commit

---
+++ Landmarks-dev/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks-dev
+//
+//  Created by fkk on 2025/4/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
---
+++ Landmarks-dev/Views/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks-dev
+//
+//  Created by fkk on 2025/4/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
